c55801acd0db532711db155663c01fbc010c902d
Kriti Rai <kriti.rai@gmail.com>
2017-11-12 04:47:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@ board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]# Define 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"# out the current state.
-  puts " #{board[3]} | #{board[4]} | #{board[5]} " 
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
